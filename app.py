d378bc1ce59008161da71eea6811591d83149894
Chandan <chandankr014@gmail.com>
2023-08-18 10:34:31 +0530

    build model

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-from flask import Flask
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+# defining endpoints
+@app.get("/")
+def homepage():
+    return "WELCOME TO FASTAPI"
+
+@app.get("/hello/{name}")
+def homepage(name):
+    return f"Hello user {name}"
+
+
+
+if __name__=="__main__":
+    uvicorn.run("app:app")
