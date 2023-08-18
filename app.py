0a2f07fddb9fe2f0ff9802f146741d4837b96bd8
Chandan <chandankr014@gmail.com>
2023-08-18 13:31:58 +0530

    updates epochs

--- app.py
+++ app.py
@@ -1,18 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile   
 import uvicorn
+import numpy as np
+from PIL import Image
+from io import BytesIO
+import tensorflow as tf
+
 
 app = FastAPI()
 
-# defining endpoints
+CLASSES = ["Potato___Early_blight", "Potato___healthy", "Potato___Late_blight"]
+
+MODEL = tf.keras.models.load_model('Models/1')
+
+
+## defining endpoints
 @app.get("/")
 def homepage():
     return "WELCOME TO FASTAPI"
 
-@app.get("/hello/{name}")
-def homepage(name):
-    return f"Hello user {name}"
+def read_image(data):
+    image = np.array(Image.open(BytesIO(data)))
+    return image
 
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    
+    image = read_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    prediction = MODEL.predict(img_batch)
+    index = np.argmax(prediction[0])
+    predicted_cls = CLASSES[index]
+    confidence = float(np.max(prediction[0]))
 
+    return {
+        "class": predicted_cls,
+        "confidence": confidence
+    }
 
+# main function
 if __name__=="__main__":
-    uvicorn.run("app:app")
+    uvicorn.run("app:app", reload=True)
